bc77c10ad2735740e07999219c5e4e975d5cea0a
Anastasia <sysoewa.nastia2017@yandex.ru>
2022-07-26 21:43:21 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿void CreateSecondArray(string[] array)
+{
+    string[] newarray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[i] = array[i]; ;
+            Console.WriteLine(newarray[i]);
+        }
+    }
+    Console.WriteLine();
+}
+
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)" };
+
+Print(array);
+CreateSecondArray(array);
